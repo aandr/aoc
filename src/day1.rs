1c9b57176863b4ff707f5f5b5921857c76677a24
Alek Andreev <alek@zvuk.net>
2022-12-01 13:59:54 +0000

    Flatten insertion sort ifs

--- src/day1.rs
+++ src/day1.rs
@@ -5,9 +5,7 @@ mod utils;
 
 extern crate test;
 use lazy_static::lazy_static;
-use std::{
-    intrinsics::{unlikely, likely},
-};
+use std::intrinsics::{likely, unlikely};
 
 lazy_static! {
     static ref INPUT: String = utils::get_puzzle_input(1);
@@ -17,10 +15,10 @@ pub fn main() {
     let mut current_elf: u32 = 0;
     let mut top3 = [0, 0, 0];
 
-    for row in INPUT.split('\n') {
+    for row in INPUT.split("\n") {
         if unlikely(row.is_empty()) {
-          insert_sort3(&mut top3, current_elf);
-          current_elf = 0;
+            insert_sort3(&mut top3, current_elf);
+            current_elf = 0;
         } else {
             let Ok(row_value) = row.parse::<u32>() else {
                 panic!("Failed to parse row: '{}'", row);
@@ -30,25 +28,21 @@ pub fn main() {
     }
     insert_sort3(&mut top3, current_elf);
 
-    println!("Max sum: {}", top3[0]);
+    println!("Max sum: {}", top3[2]);
     println!("Max 3 sum: {}", top3.iter().sum::<u32>());
 }
 
 fn insert_sort3<T: PartialOrd + Copy>(arr: &mut [T; 3], newval: T) {
-  if likely(newval > arr[0]) {
-    if unlikely(newval > arr[1]) {
-      if unlikely(newval > arr[2]) {
+    if unlikely(newval > arr[2]) {
         arr[0] = arr[1];
         arr[1] = arr[2];
         arr[2] = newval;
-      } else {
+    } else if unlikely(newval > arr[1]) {
         arr[0] = arr[1];
         arr[1] = newval;
-      }
-    } else {
-      arr[0] = newval;
+    } else if likely(newval > arr[0]) {
+        arr[0] = newval;
     }
-  }
 }
 
 #[bench]
