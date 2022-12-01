c87d77c421f130f79279e32b7d9869bddd4ab5f0
Alek Andreev <alek@zvuk.net>
2022-12-01 11:05:33 +0000

    Remove the vector for all elves

--- src/day1.rs
+++ src/day1.rs
@@ -5,48 +5,48 @@ mod utils;
 
 extern crate test;
 use lazy_static::lazy_static;
-use std::intrinsics::{unlikely, likely};
+use std::{
+    intrinsics::{likely, unlikely},
+    thread::current,
+};
 
 lazy_static! {
     static ref INPUT: String = utils::get_puzzle_input(1);
 }
 
 pub fn main() {
-    let mut sums = vec![0];
+    let mut current_elf: u32 = 0;
+    // Keep a running list of the top 4, whereas the last element will be swapped by the latest elf.
+    let mut top4 = [0, 0, 0, 0];
 
     for row in INPUT.split('\n') {
         if unlikely(row.is_empty()) {
-            sums.push(0)
+            top4[3] = current_elf;
+            sort4(&mut top4);
+            current_elf = 0;
         } else {
             let Ok(row_value) = row.parse::<u32>() else {
                 panic!("Failed to parse row: '{}'", row);
             };
-            *sums.last_mut().unwrap() += row_value;
+            current_elf += row_value;
         }
     }
+    println!("Max sum: {}", top4[0]);
+    println!("Max 3 sum: {}", top4[0] + top4[1] + top4[2]);
+}
 
-    // The last 3 elements contains the top 3. Element 0 is a buffer.
-    let mut top4 = [0, 0, 0, 0];
-
-    for sum in sums {
-        top4[0] = sum;
-        // Inspired by LLVM's sort4.
-        // Unfortunately branchless (ie swap with self) works slower than branched.
-        cond_swap(&mut top4, 0, 2);
-        cond_swap(&mut top4, 1, 3);
-        cond_swap(&mut top4, 0, 1);
-        cond_swap(&mut top4, 2, 3);
-        cond_swap(&mut top4, 1, 2);
-    }
-
-    println!("Max sum: {}", top4[3]);
-    println!("Max 3 sum: {}", top4[1] + top4[2] + top4[3]);
+fn sort4<T: PartialOrd>(arr: &mut [T; 4]) {
+    cond_swap(arr, 0, 2);
+    cond_swap(arr, 1, 3);
+    cond_swap(arr, 0, 1);
+    cond_swap(arr, 2, 3);
+    cond_swap(arr, 1, 2);
 }
 
 #[inline]
 fn cond_swap<T: PartialOrd, const S: usize>(arr: &mut [T; S], x: usize, y: usize) {
     // Branch prediction hint.
-    if likely(arr[x] > arr[y]) {
+    if likely(arr[x] < arr[y]) {
         arr.swap(x, y);
     }
 }
